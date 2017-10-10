ec7eb752d36b6b12aaad4cec3603070bd42480a8
esther <estheralvaro44@gmail.com>
2017-10-10 14:32:20 -0500

    lyft principal

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './estherUp.css';
 import {
 	BrowserRouter,
 	Route,
@@ -7,30 +8,61 @@ import {
 	Redirect
 } from 'react-router-dom'
 
-
 const SignUp = ({model}) => {
 	return (
-		<div>
-			<header>
-				<nav className="navbar navbar-light text-center">
-					<a href="javascript:window.history.back();">
-						<i
-							className="fa fa-angle-left fa-3x"
-							aria-hidden="true"/>
-					</a>
-					<div className="navbar-brand">
-						<h3>
-							Sign Up
-						</h3>
+		<form>
+		<div id="contacto">
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-3">
+						<a href="index.html"><span className="glyphicon glyphicon-chevron-left" /></a>
 					</div>
-				</nav>
-			</header>
-
-			<NavLink to={"/signup-form"}
-			         className="btn btn-lyft btn-lg btn-block">
-				Next
-			</NavLink>
+					<div className="col-md-9">
+						<h2 className="Sign1">Sign Up</h2>
+					</div>
+					<div className="col-md-12">
+						<h5>Join now for tree ride Credit</h5>
+					</div>
+					
+					<div className="row vacio">
+						<div className="col-lg-6">
+							<div className="input-group">
+								<div className="input-group-btn">
+									<button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Paises<span className="caret" /></button>
+								</div>
+								<input type="number" className="form-control" required/>
+							</div>
+							<h5> We'll send a text to verity your phone</h5>
+						</div>
+					</div>
+					<div className="col-md-12">
+					 	<NavLink to={"/signup-form"}
+		 	         className="btn btn-lyft btn-lg btn-block">
+		 		Next
+		 	</NavLink>
+					</div>
+				</div>
+			</div>
 		</div>
+		</form>
+		// <div>
+		// 		<nav className="navbar navbar-light text-center">
+		// 			<a href="javascript:window.history.back();">
+		// 				<i
+		// 					className="fa fa-angle-left fa-3x"
+		// 					aria-hidden="true"/>
+		// 			</a>
+		// 			<div className="navbar-brand">
+		// 				<h3>
+		// 					Sign Up
+		// 				</h3>
+		// 			</div>
+		// 		</nav>
+		// 	<NavLink to={"/signup-form"}
+		// 	         className="btn btn-lyft btn-lg btn-block">
+		// 		Next
+		// 	</NavLink>
+		// </div>
 	);
 }
 
