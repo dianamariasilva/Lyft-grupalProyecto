cc7897e38497c5d8a460fb5e3580f60b57323666
carina120794@gmail.com <you@example.com>
2017-10-10 14:00:58 -0500

    formulario de signupform

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,5 +1,6 @@
 
 import React, {Component} from 'react';
+import './eve.css';
 import {
 	BrowserRouter,
 	Route,
@@ -30,23 +31,44 @@ class SignUpForm extends Component {
 		return (
 			<div>
 				<header>
-					<nav className="navbar navbar-light text-center">
+					<nav className="navbar navbar-light">
 						<a href="javascript:window.history.back();">
 							<i
 								className="fa fa-angle-left fa-3x"
 								aria-hidden="true" />
 						</a>
-						<div className="navbar-brand">
-							<h3>
-								Sign Up Form
-							</h3>
+						<div className="navbar-brand ">
+							
+							<h1 className="col-md-12 col-sm-12 titulo " id="titulo_2">Sing up</h1>					
+							<h3 className="col-md-12 col-sm-12 titulo" id="subtitulo_2">Join now for free ride credit</h3>
 
 						</div>
 					</nav>
 				</header>
-				<section className="container-fluid form">
 
-					<label className="form-check-label">
+				<div className="form-box">
+                    <form role="form" id="contact-form">                  
+                        <div className="form-group">
+                        <div id="nameError" className="sr-only" role="alert"></div>
+                        <label for="form-name-field" className="sr-only">Name</label>
+                            <div className="input-group">
+                                <div className="input-group-addon"><span className="glyphicon glyphicon-user"></span></div>
+                                <input  class="form-control" type="text" placeholder="Name"/>
+				            </div>
+                        </div>
+                        <div className="form-group">
+                        <div id="emailError" className="sr-only" role="alert"></div>
+                        <label for="form-email-field" className="sr-only">Email</label>
+                            <div className="input-group">
+                                <div className="input-group-addon"><span className="glyphicon glyphicon-envelope"></span></div>
+                                <input  class="form-control" placeholder="Email" type="email" id="myEmail" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"/>
+                            </div>
+                        </div>
+						</form>
+					</div>
+
+				<section className="container-fluid form text-center">
+					<label className="form-check-label ">
 						<input className="form-check-input" id="agreeUser" type="checkbox" onChange={onInputChange}/>
 						I agree to Lyft's <a href="lyft.com"> Terms of Service</a>
 					</label>
@@ -58,7 +80,7 @@ class SignUpForm extends Component {
 								className="btn btn-lg btn-block btn-lyft">Next</NavLink>
 							:
 							<button
-								className="btn btn-lg btn-block btn-lyft disabled">Next</button>
+							className="btn btn-lg btn-block btn-lyft disabled">Next</button>
 
 					}
 
